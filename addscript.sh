3c2d88dcf73070783176f8839e6fa01905c7b220
shuoscaryo <shuoscaryo@gmail.com>
2023-10-30 18:24:40 +0100

    .

--- addscript.sh
+++ addscript.sh
@@ -8,13 +8,13 @@ if [ $# -ne 1 ]; then
 	exit 1
 fi
 
-echo $(readlink "$1")
-script_dir=$(dirname $(readlink "$1"))
-file_name="${1%.sh}"
+script_dir=$(dirname $(readlink -f "$1"))
+file_name=$(basename "${1%.sh}")
 install_dir=~/.local/bin
 
-echo "script dir" $script_dir
-echo "file_name" $file_name
-exit 0
+function realpath {
+    [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
+}
+
 ln -s $script_dir/$file_name.sh $install_dir/$file_name
 chmod +x $install_dir/$file_name
