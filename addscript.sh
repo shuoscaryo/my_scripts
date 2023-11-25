ecf259e59d4e26ef98960c07db8f715bcd594080
shuoscaryo <shuoscaryo@gmail.com>
2023-11-25 14:03:14 +0100

    addscript: added question when file is not executable and check for when link already exists

--- addscript.sh
+++ addscript.sh
@@ -3,8 +3,19 @@
 #When running it the first time, use bash ./addscript.sh addscript.sh to add the script in the path
 #Then you can do : addscript script.sh
 
+#This variable can be changed to another path
+install_dir=~/.local/bin
+
 if [ $# -ne 1 ]; then
 	echo "Usage: addscript script_name.sh"
+	exit 1
+fi
+
+script_dir=$(cd $(dirname "$1") && pwd)
+file_name=$(basename "${1%.*}")
+
+if [ -f $install_dir/$file_name ]; then
+	echo "Link $install_dir/$file_name already exists"
 	exit 1
 fi
 
@@ -12,21 +23,30 @@ if [ ! -f $1 ]; then
 	echo "File $1 does not exist"
 	exit 1
 fi
+
 if [ ! -x $1 ]; then
 	echo "File $1 is not executable"
-	exit 1
+	while true; do
+		read -p "Do you want to add execute flag to the script? [y/n]" answer
+		if [[ "$answer" =~ (y|Y) || "$answer" =~ (n|N) ]]; then
+			break
+		fi
+		echo "Wrong answer!"
+	done
+	if [[ "$answer" =~ (y|Y) ]]; then
+		echo "adding execution permission to file $1"
+		chmod +x $1
+	else
+		echo "That's sad :( Ending program"
+		exit 0
+	fi
 fi
+
 if [ -h $1 ]; then
 	echo "File $1 is already a symbolic link"
 	exit 1
 fi
 
-cd $(dirname "$1")
-script_dir=$(pwd)
-cd - > /dev/null 2>&1
-
-file_name=$(basename "${1%.sh}")
-install_dir=~/.local/bin
-
 ln -s $script_dir/$file_name.sh $install_dir/$file_name
 chmod +x $install_dir/$file_name
+echo "Script $file_name added to path"
