3395eead1a3d98262748e7bb2fea01ee137873a7
oscar <shuoscaryo@gmail.com>
2023-11-28 16:11:38 +0100

    [main]<2023-11-28-16:11> quicksave

--- quicksave/quicksave.sh
+++ quicksave/quicksave.sh
@@ -2,9 +2,9 @@
 
 set -e
  
-date=$(date +"%Y-%m-%d-%H:%M")
-curr_branch=$(git branch | grep "\*" | cut -d ' ' -f2 )
 git add --all
 git status -s
+date=$(date +"%Y-%m-%d-%H:%M")
+curr_branch=$(git branch | grep "\*" | cut -d ' ' -f2 )
 git commit -m "[$curr_branch]<$date> quicksave"
 git push
