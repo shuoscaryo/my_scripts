5ac38bf08c4402de6827c49dc4248d9d1d478e3e
oscar <shuoscaryo@gmail.com>
2023-11-29 16:00:46 +0100

    [main]<2023-11-29-16:00> quicksave con mensaje!

--- quicksave/quicksave.sh
+++ quicksave/quicksave.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 set -e
- 
+
+if [ $# -ge 1 ]; then
+	MSG=$*
+else
+	MSG="quicksave"
+fi
+
 git add --all
 git status -s
 date=$(date +"%Y-%m-%d-%H:%M")
 curr_branch=$(git branch | grep "\*" | cut -d ' ' -f2 )
-git commit -m "[$curr_branch]<$date> quicksave"
+git commit -m "[$curr_branch]<$date> $MSG"
 git push
