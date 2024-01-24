86dd82aa542a4905f11e554509bd014079160d2f
shuoscaryo <shuoscaryo@gmail.com>
2024-01-24 12:54:57 +0100

    [main]<2024-01-24-12:54> updmake: files are now saved relative to the location of the makefile when using a parameter (updmake src saves the files as src/file instead of file), and now multiple folders can be selected to search for files (updmake src class saves files src/* and class/*)

--- updmake/updmake.sh
+++ updmake/updmake.sh
@@ -1,29 +1,53 @@
 #!/bin/bash
 
+# SETUP THE SCRIPT
+# Get the directory where this script is located
+# If it is a symlink, get the directory where the symlink points to
+if [ -h "$0" ]; then
+	script_dir=$(dirname $(readlink "$0"))
+# If it is not a symlink, get the directory where the script is located
+else
+	script_dir=$(cd $(dirname $0) && pwd)
+fi
+# Compile the program that will replace the src in the makefile
+if ! make -C $script_dir > /dev/null 2>&1; then
+	echo "ERROR: Could not execute Make in the directory of the script ($script_dir)."
+	exit 1
+fi
+
+# EXECUTE THE SCRIPT
+# Don't execute if there is no makefile in the current directory
 if ! [ -f "Makefile" ]; then
 	echo "ERROR: No makefile found in current directory."
 	exit 1
 fi
 
-if [ $# -ge 1 ]; then
-	file_dir=$(cd $1 && pwd)
+current_dir=$(pwd)
+
+if [ $# -eq 0 ]; then
+	files=$(find $current_dir -type f \( -name "*.c" -o -name "*.cpp" \) | sed "s|$current_dir/||g")
 else
-	file_dir=$(pwd)
+	for i in $@; do
+		if ! [ -d $i ]; then
+			echo "ERROR: $i is not a directory."
+			continue
+		fi
+		file_dir=$(cd $i && pwd)
+		# Add the files found to the variable and remove the current directory from the path
+		# Also add a newline at the end of each file
+		files+="$(find $file_dir -type f \( -name "*.c" -o -name "*.cpp" \) | sed "s|$current_dir/||g")"$'\n'
+	done
 fi
 
-if [ -h "$0" ]; then
-	script_dir=$(dirname $(readlink "$0"))
-else
-	script_dir=$(cd $(dirname $0) && pwd)
-fi
+# Sort and remove duplicates
+files=$(echo "$files" | sort -u)
 
-files=$(find $file_dir -type f \( -name "*.c" -o -name "*.cpp" \) | sed "s|$file_dir/||g")
-
+# Print the files that will be replaced
 echo "new files:"
 for i in $files; do
 	echo -e "\t$i"
 done
 
-make -C $script_dir > /dev/null 2>&1
+# Replace the src in the makefile
 echo "Replacing src in Makefile"
 $script_dir/replace_src $files
