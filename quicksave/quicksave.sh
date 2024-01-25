8143bf2c23d957ab74c032f3689d29cb479b0e96
shuoscaryo <shuoscaryo@gmail.com>
2024-01-25 23:41:23 +0100

    [main]<2024-01-25-23:41> Quicksave: Added option -n that does not execute "git add --all" before pushing

--- quicksave/quicksave.sh
+++ quicksave/quicksave.sh
@@ -2,13 +2,29 @@
 
 set -e
 
+GITADD=true
+while getopts ":n" opt; do
+	case $opt in
+		n)
+			GITADD=false
+			;;
+		\?)
+			echo "Invalid option: -$OPTARG" >&2
+			;;
+	esac
+done
+
+shift $((OPTIND - 1))
+
 if [ $# -ge 1 ]; then
 	MSG=$*
 else
 	MSG="quicksave"
 fi
 
-git add --all
+if [ $GITADD = true ]; then
+	git add --all
+fi
 git status -s
 date=$(date +"%Y-%m-%d-%H:%M")
 curr_branch=$(git branch | grep "\*" | cut -d ' ' -f2 )
